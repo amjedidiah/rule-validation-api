c5c2d1eded7aa9050785bc1e287600524a5c91b1
amjedidiah <imunacode@gmail.com>
2021-01-28 00:21:40 +0100

    chore: Completed express app setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Starting...');
+// Module imports
+const express = require('express');
+
+// Initiate our app
+const app = express();
+const port = process.env.PORT || 8008;
+
+// Listen on Port 8008
+app.listen(port, () => console.log(`App started on ${port}`));
+
